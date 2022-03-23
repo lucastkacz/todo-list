9ab55a8d760e8fcf6b6d8060c6eb72a4cd28bba2
lucastkacz <lucastkacz@gmail.com>
2022-03-23 13:47:16 -0300

    Fin

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'docs'),
     },
 };
+
+// 'docs' used to host website on github
